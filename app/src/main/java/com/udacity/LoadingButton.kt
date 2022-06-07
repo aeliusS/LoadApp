e4ae1cf58673161ff09362baf3dd18a0d310c6fb
Keld <keldarch@amalia.anonaddy.com>
2022-06-07 14:54:06 -0400

    added loading circle animation to custom button

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,5 +1,7 @@
 package com.udacity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
@@ -26,25 +28,59 @@ class LoadingButton @JvmOverloads constructor(
         color = ResourcesCompat.getColor(resources, R.color.white, null)
     }
 
+    private var buttonText = resources.getString(R.string.button_download)
+    private var textPixelSize = textPaint.measureText(buttonText)
+    private var percentageLoaded = -1.0f
+    // determines the size of the loading circle
+    private val radius = 0.45f
+    // determines how far way the loading circle will be placed after the text
+    private val circleOffset = 10f
+
     private val buttonPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         color = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
     }
 
-    private var rectF = RectF(0.0f, 0.0f, 0.0f, 0.0f)
+    private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        strokeWidth = 25F
+        color = ResourcesCompat.getColor(resources, R.color.colorAccent, null)
+    }
+
+    private var buttonRectF = RectF()
+    private var arcRectF = RectF()
 
     // use this as a pulse to animate the circle in the button
     // https://medium.com/mindorks/android-property-animation-the-valueanimator-4ca173567cdb
-    private val valueAnimator = ValueAnimator()
+    private val valueAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
+        duration = 2000
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                percentageLoaded = -1.0f
+                Log.d("LoadingButton", "Finished animation")
+            }
+        })
+        addUpdateListener { updatedAnimation ->
+            percentageLoaded = updatedAnimation.animatedValue as Float
+            invalidate()
+        }
+    }
 
     // use the delegates observable to react to the different button states
     private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { prop, old, new ->
         when (new) {
-            ButtonState.Loading -> contentDescription = resources.getString(R.string.button_loading)
-            ButtonState.Completed -> contentDescription = resources.getString(R.string.button_download)
+            ButtonState.Loading -> {
+                buttonText = resources.getString(R.string.button_loading)
+            }
+            ButtonState.Completed -> {
+                buttonText = resources.getString(R.string.button_download)
+            }
             else -> {}
         }
-
+        textPixelSize = textPaint.measureText(buttonText)
+        contentDescription = buttonText
+        updateLoadingCircleDimen()
+        invalidate()
     }
 
 
@@ -56,8 +92,8 @@ class LoadingButton @JvmOverloads constructor(
     override fun performClick(): Boolean {
         super.performClick()
 
+        if (valueAnimator.isRunning) valueAnimator.end() else valueAnimator.start()
         buttonState = ButtonState.Clicked
-        invalidate()
         return true
     }
 
@@ -66,24 +102,39 @@ class LoadingButton @JvmOverloads constructor(
         super.onDraw(canvas)
         drawButton(canvas)
         drawButtonText(canvas)
-        Log.d("LoadingButton", "Finished draw")
+        drawLoadingCircle(canvas)
+        // Log.d("LoadingButton", "Finished draw")
+        // logDimensions()
+    }
+
+    private fun logDimensions() {
+        Log.d("LoadingButton", "Arc: ${arcRectF.toShortString()}")
+        Log.d("LoadingButton", "Button: ${buttonRectF.toShortString()}")
+        Log.d("LoadingButton", "Height: $heightSize Width: $widthSize")
+        Log.d("LoadingButton", "ButtonRectBottom: ${buttonRectF.bottom}")
+    }
+
+    private fun drawLoadingCircle(canvas: Canvas) {
+        if (percentageLoaded == -1.0f) return
+        canvas.drawArc(arcRectF, 0F, 360F * percentageLoaded, true, circlePaint)
     }
 
     private fun drawButton(canvas: Canvas) {
-        rectF.bottom = widthSize.toFloat()
-        canvas.drawRect(rectF, buttonPaint)
+        canvas.drawRect(buttonRectF, buttonPaint)
     }
 
     private fun drawButtonText(canvas: Canvas) {
-        canvas.drawText(
-            when (buttonState) {
-                ButtonState.Loading -> context.getString(R.string.button_loading)
-                else -> context.getString(R.string.button_download)
-            },
-            xTextCenter,
-            yTextCenter,
-            textPaint
-        )
+        canvas.drawText(buttonText, xTextCenter, yTextCenter, textPaint)
+    }
+
+    private fun updateLoadingCircleDimen() {
+        val halfWidth = widthSize.toFloat() / 2
+        val halfHeight = heightSize.toFloat() / 2
+        arcRectF.top = halfHeight - (halfHeight * radius)
+        arcRectF.bottom = halfHeight + (halfHeight * radius)
+
+        arcRectF.left = halfWidth + (textPixelSize/2) + circleOffset
+        arcRectF.right = arcRectF.left + (2 * halfHeight * radius)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -92,8 +143,10 @@ class LoadingButton @JvmOverloads constructor(
         yTextCenter = (h / 2).toFloat() - ((textPaint.descent() + textPaint.ascent()) / 2)
 
         // update the button rectangle dimensions
-        rectF.right = widthSize.toFloat()
-        rectF.bottom = heightSize.toFloat()
+        buttonRectF.right = widthSize.toFloat()
+        buttonRectF.bottom = heightSize.toFloat()
+
+        updateLoadingCircleDimen()
     }
 
     // https://medium.com/@quiro91/custom-view-mastering-onmeasure-a0a0bb11784d
