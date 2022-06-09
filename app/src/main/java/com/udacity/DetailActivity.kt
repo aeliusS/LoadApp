851747dc5b6aca09ad652fbd8c259443fa5f19cd
Keld <keldarch@amalia.anonaddy.com>
2022-06-09 14:15:12 -0400

    updated notification message to be dynamic. updated to pass data to the detail activity

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.udacity
 
 import android.app.NotificationManager
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.udacity.databinding.ActivityDetailBinding
 
@@ -16,6 +18,15 @@ class DetailActivity : AppCompatActivity() {
         // dismiss the notification
         val notificationManager = getSystemService(NotificationManager::class.java)
         notificationManager.cancel(NOTIFICATION_ID)
+
+        // get the file name and status
+        val extras = intent.extras
+        if (extras != null) {
+            val fileName = extras.getString("File") ?: "Unknown file"
+            val status = extras.getString("Status") ?: "Unknown status"
+
+            Log.d("DetailActivity","File name: $fileName. Status: $status")
+        }
     }
 
     companion object {
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.net.Uri
 import android.os.Build
@@ -17,7 +18,11 @@ import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.lifecycle.lifecycleScope
 import com.udacity.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +33,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+
+    private lateinit var prefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,13 +48,24 @@ class MainActivity : AppCompatActivity() {
             getString(R.string.notification_channel_name)
         )
 
+        prefs = application.getSharedPreferences("com.udacity", Context.MODE_PRIVATE)
+
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         binding.contentMain.customButton.setOnClickListener {
             if (!validRadioOption()) return@setOnClickListener
             binding.contentMain.customButton.setButtonLoading()
-            download()
+
+            // place the current file to be downloaded into shared preference to be retrieved later
+            val selectedIndex = binding.contentMain.radioGroup.checkedRadioButtonId
+            val radioButton = findViewById<RadioButton>(selectedIndex)
+            lifecycleScope.launch {
+                saveRadioOption(selectedIndex, radioButton.text.toString())
+            }
+
+            download(selectedIndex)
         }
+
     }
 
     private fun validRadioOption(): Boolean {
@@ -58,6 +76,24 @@ class MainActivity : AppCompatActivity() {
         }
         return true
     }
+
+    private suspend fun saveRadioOption(radioId: Int, fileName: String) =
+        withContext(Dispatchers.IO) {
+            with(prefs.edit()) {
+                putInt(SELECTED_RADIO_ID, radioId)
+                putString(SELECTED_FILE_NAME, fileName)
+                apply()
+            }
+        }
+
+    private suspend fun getRadioOption(): RadioOption =
+        withContext(Dispatchers.IO) {
+            RadioOption(
+                prefs.getInt(SELECTED_RADIO_ID, 0),
+                prefs.getString(SELECTED_FILE_NAME, null) ?: "Unknown file"
+            )
+
+        }
 
     private fun createChannel(channelId: String, channelName: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -80,19 +116,26 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context, intent: Intent) {
             val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
             if (id == downloadID) {
-                Log.i("MainActivity", "Download finished")
-                notificationManager.sendNotification(
-                    context.getText(R.string.notification_description).toString()
-                )
+                Log.d("MainActivity", "Download finished")
+                val statusString = getDownloadStatus(downloadID)
+                Log.d("MainActivity", "Status: $statusString")
+
+                lifecycleScope.launch {
+                    val (radioId, fileName) = getRadioOption()
+                    notificationManager.sendNotification(
+                        notifyMessageBody(radioId),
+                        fileName,
+                        statusString
+                    )
+                }
                 // binding.contentMain.customButton.setButtonCompletedDownload()
             }
         }
     }
 
-    private fun download() {
-        downloadURL()
+    private fun download(selectedIndex: Int) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(downloadURL(selectedIndex)))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -104,21 +147,61 @@ class MainActivity : AppCompatActivity() {
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
-    // TODO: return URL string from string resource
-    private fun downloadURL() {
-        when (binding.contentMain.radioGroup.checkedRadioButtonId) {
-            R.id.glide_radio_button -> Log.d("MainActivity", "Glide was selected")
-            R.id.load_app_radio_button -> Log.d("MainActivity", "LoadApp was selected")
-            R.id.retrofit_radio_button -> Log.d("MainActivity", "Retrofit was selected")
+    private fun downloadURL(selectedIndex: Int): String {
+        return when (selectedIndex) {
+            R.id.glide_radio_button -> getString(R.string.glide_url)
+            R.id.load_app_radio_button -> getString(R.string.load_app_url)
+            R.id.retrofit_radio_button -> getString(R.string.retrofit_url)
+            else -> ""
         }
     }
 
-    private fun NotificationManager.sendNotification(messageBody: String) {
-        val checkStatusIntent = Intent(applicationContext, DetailActivity::class.java)
+    private fun notifyMessageBody(radioId: Int): String {
+        return when (radioId) {
+            R.id.glide_radio_button -> getString(R.string.glide_option_prettify)
+            R.id.load_app_radio_button -> getString(R.string.load_app_option_prettify)
+            R.id.retrofit_radio_button -> getString(R.string.retrofit_option_prettify)
+            else -> getString(R.string.custom_option_prettify)
+        }
+    }
+
+    /**
+     * Returns the download status as a String
+     * Returns either Failed or Success
+     * */
+    private fun getDownloadStatus(id: Long): String {
+        val downloadedFile = DownloadManager.Query()
+            .setFilterById(id)
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val cursor = downloadManager.query(downloadedFile)
+
+        // return if the query returned with no rows
+        if (cursor.isAfterLast) return "Failed"
+        cursor.moveToFirst()
+
+        val column = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+        var status = DownloadManager.STATUS_FAILED
+        if (column > -1) status = cursor.getInt(column)
+
+        return when (status) {
+            DownloadManager.STATUS_SUCCESSFUL -> "Success"
+            else -> "Failed"
+        }
+    }
+
+    private fun NotificationManager.sendNotification(
+        messageBody: String,
+        fileName: String,
+        status: String
+    ) {
+        val downloadStatusIntent = Intent(applicationContext, DetailActivity::class.java)
+        downloadStatusIntent.putExtra("File", fileName)
+        downloadStatusIntent.putExtra("Status", status)
+
         pendingIntent = PendingIntent.getActivity(
             applicationContext,
             NOTIFICATION_ID,
-            checkStatusIntent,
+            downloadStatusIntent,
             PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         )
         // the custom button
@@ -143,10 +226,12 @@ class MainActivity : AppCompatActivity() {
         notify(NOTIFICATION_ID, builder.build())
     }
 
+    data class RadioOption(var radioId: Int, var fileName: String)
+
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val NOTIFICATION_ID = 0
+        private const val SELECTED_RADIO_ID = "SELECTED_RADIO_ID"
+        private const val SELECTED_FILE_NAME = "SELECTED_FILE_NAME"
     }
 
 }
